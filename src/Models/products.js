404afb7317ecbeca0ad4a8e2f7898e02e8d6a8e5
Matheus Loubach <34719454+Matheus-Loubach@users.noreply.github.com>
2023-12-16 22:49:36 -0300

    Feat: registerProducts

--- src/Models/products.js
+++ src/Models/products.js
@@ -2,58 +2,63 @@ const mongoose = require('mongoose');
 
 const productsSchema = new mongoose.Schema({
 
-    idUser:{
-        type:String,
-        required:true
-    },
-    
-    nameProducts:{
-        type:String,
-        required:true
+    idUser: {
+        type: String,
+        required: true
     },
 
-    description:{
-        type:String,
-        required:false
+    nameUser: {
+        type: String,
+        require: true
     },
 
-    category:{
-        type:String,
-        required:true
-    }, 
-
-    link:{
-        type:String,
-
-    },
-    codeSKU:{
-        type:String,
-
-    },
-    mark:{
-        type:String,
+    nameProducts: {
+        type: String,
+        required: true
     },
 
-    stock:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: false
     },
 
-    price:{
-        type:String,
-        required:true
+    category: {
+        type: String,
+        required: true
     },
-    localization:{
-        type:String,
+
+    link: {
+        type: String,
+
+    },
+    codeSKU: {
+        type: String,
+
+    },
+    mark: {
+        type: String,
+    },
+
+    stock: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: String,
+        required: true
+    },
+    localization: {
+        type: String,
 
     },
 
-}, {timestamps:true} )
+}, { timestamps: true })
 
 
 const Products = mongoose.model('Products', productsSchema);
 module.exports = {
-    Products, 
+    Products,
     productsSchema
 }
 
